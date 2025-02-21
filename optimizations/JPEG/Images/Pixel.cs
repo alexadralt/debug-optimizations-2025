ef8e7c3621adeb47820715d6e793632094079d14
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2025-02-22 00:06:14 +0500

    huge boost: pixel is now a struct and pixelFormat is now an enum

--- optimizations/JPEG/Images/Pixel.cs
+++ optimizations/JPEG/Images/Pixel.cs
@@ -3,10 +3,8 @@ using System.Linq;
 
 namespace JPEG.Images;
 
-public class Pixel
+public readonly struct Pixel
 {
-	private readonly PixelFormat format;
-
 	public Pixel(double firstComponent, double secondComponent, double thirdComponent, PixelFormat pixelFormat)
 	{
 		if (!new[] { PixelFormat.RGB, PixelFormat.YCbCr }.Contains(pixelFormat))
@@ -14,35 +12,32 @@ public class Pixel
 		format = pixelFormat;
 		if (pixelFormat == PixelFormat.RGB)
 		{
-			r = firstComponent;
-			g = secondComponent;
-			b = thirdComponent;
+			value1 = firstComponent;
+			value2 = secondComponent;
+			value3 = thirdComponent;
 		}
 
 		if (pixelFormat == PixelFormat.YCbCr)
 		{
-			y = firstComponent;
-			cb = secondComponent;
-			cr = thirdComponent;
+			value1 = firstComponent;
+			value2 = secondComponent;
+			value3 = thirdComponent;
 		}
 	}
 
-	private readonly double r;
-	private readonly double g;
-	private readonly double b;
+	private readonly PixelFormat format;
+	private readonly double value1;
+	private readonly double value2;
+	private readonly double value3;
 
-	private readonly double y;
-	private readonly double cb;
-	private readonly double cr;
-
-	public double R => format == PixelFormat.RGB ? r : (298.082 * y + 408.583 * Cr) / 256.0 - 222.921;
+	public double R => format == PixelFormat.RGB ? value1 : (298.082 * value1 + 408.583 * Cr) / 256.0 - 222.921;
 
 	public double G =>
-		format == PixelFormat.RGB ? g : (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
+		format == PixelFormat.RGB ? value2 : (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
 
-	public double B => format == PixelFormat.RGB ? b : (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
+	public double B => format == PixelFormat.RGB ? value3 : (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
 
-	public double Y => format == PixelFormat.YCbCr ? y : 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
-	public double Cb => format == PixelFormat.YCbCr ? cb : 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
-	public double Cr => format == PixelFormat.YCbCr ? cr : 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
+	public double Y => format == PixelFormat.YCbCr ? value1 : 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
+	public double Cb => format == PixelFormat.YCbCr ? value2 : 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
+	public double Cr => format == PixelFormat.YCbCr ? value3 : 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
 }
